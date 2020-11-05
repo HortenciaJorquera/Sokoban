f4cc23a9cc0a4f336ec04c12ccea8d7ebb532247
Hortencia Jorquera <hortencia.pj@gmail.com>
2020-11-05 16:13:58 +0100

    prueba 3

--- src/game.js
+++ src/game.js
@@ -18,10 +18,10 @@ class Game {
       }
 
     preload(){
-        this.wallImg=loadImage('/assets/images/wall.png');
-        this.storageLocationImg=loadImage('/assets/images/storage-location.png');
-        this.grassImg=loadImage('/assets/images/grass.png');
-        this.floorImg=loadImage('/assets/images/floor.png');
+        this.wallImg=loadImage('assets/images/wall.png');
+        this.storageLocationImg=loadImage('assets/images/storage-location.png');
+        this.grassImg=loadImage('assets/images/grass.png');
+        this.floorImg=loadImage('assets/images/floor.png');
     }
 
     setup(){
